b7def6932c75b713b07e56aeec31641c73f0a9dd
Edwin Marte Torres <kdgpro15@gmail.com>
2019-11-09 17:32:28 -0400

    091119002 - Mejorando JS de la Tarea del Modal

--- northwind_tarea/Scripts/NW_Orders/nw_orders.js
+++ northwind_tarea/Scripts/NW_Orders/nw_orders.js
@@ -1,26 +1,21 @@
-﻿//alert("Hola Mundo");
-
-$('#orderDetails').click(function(e) {
+﻿$('#orderDetails').click(function(e) {
     var order_id = this.getAttribute("OrderID");
+    var divListadoOrdenCompra = $("#listadoOrdenCompra");
 
-    $.ajax({
-        type: "GET",
-        url: "/Orders/OrdersDetailsPV",
-        data: { id: order_id },
-        error: function () {
-            alert("No funciono :'(");
-        },
-        success: function (result) {
-            var divListadoOrdenCompra = $("#listadoOrdenCompra");
-
-            if (divListadoOrdenCompra.children().length > 0) {
-                divListadoOrdenCompra.toggle(function (e) {
-                    $(this).html(result);
-                });
-            } else {
-                divListadoOrdenCompra.html(result);
+    if (divListadoOrdenCompra.children().length > 0) {
+        divListadoOrdenCompra.toggle();
+    } else {
+        $.ajax({
+            type: "GET",
+            url: "/Orders/OrdersDetailsPV",
+            data: { id: order_id },
+            error: function() {
+                alert("No funciono :'(");
+            },
+            success: function(result) {
+                divListadoOrdenCompra.append(result);
             }
-        }
-    });
+        });
+    }
 });
 
