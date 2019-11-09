2d0ad389d0345db185b43c175ee57b36cf387e6a
Edwin Marte Torres <kdgpro15@gmail.com>
2019-11-09 18:05:04 -0400

    091119003 - Terminando Tarea Modal

--- northwind_tarea/Scripts/NW_Orders/nw_orders.js
+++ northwind_tarea/Scripts/NW_Orders/nw_orders.js
@@ -1,21 +1,19 @@
 ﻿$('#orderDetails').click(function(e) {
     var order_id = this.getAttribute("OrderID");
-    var divListadoOrdenCompra = $("#listadoOrdenCompra");
+    var modalOD = $(".orderDetailsModal");
+    var modalOrdenCompra = $(".modal-body");
 
-    if (divListadoOrdenCompra.children().length > 0) {
-        divListadoOrdenCompra.toggle();
-    } else {
-        $.ajax({
-            type: "GET",
-            url: "/Orders/OrdersDetailsPV",
-            data: { id: order_id },
-            error: function() {
-                alert("No funciono :'(");
-            },
-            success: function(result) {
-                divListadoOrdenCompra.append(result);
-            }
-        });
-    }
+    $.ajax({
+        type: "GET",
+        url: "/Orders/OrdersDetailsPV",
+        data: { id: order_id },
+        error: function() {
+            alert("No funciono :'(");
+        },
+        success: function (result) {
+            modalOD.modal();
+            modalOrdenCompra.html(result);
+        }
+    });
 });
 
